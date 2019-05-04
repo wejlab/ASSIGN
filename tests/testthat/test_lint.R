4258d1d54bd19e4079876bc21240f6dbaca8cb1c
David Jenkins <d.jenkins0@gmail.com>
2019-05-04 16:36:16 -0400

    Updated config for lintr

--- tests/testthat/test_lint.R
+++ tests/testthat/test_lint.R
@@ -1,7 +1,7 @@
 if (requireNamespace("lintr", quietly = TRUE)) {
   context("lints")
   test_that("Package Style", {
-    lintr::expect_lint_free(linters = with_defaults(
+    lintr::expect_lint_free(linters = lintr::with_defaults(
       camel_case_linter = NULL,
       snake_case_linter = NULL,
       commas_linter = NULL,
